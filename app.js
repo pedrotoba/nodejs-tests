d1e574c880e428a76ef89f2d954ad1ae39d365f7
Pedro <pedrotorres@MacBook-Pro-de-Pedro.local>
2017-06-08 17:51:14 +0200

    Preview of html

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("hola");
+var express = require('express');
+var app = express();
+
+/*app.get('/', function (req, res) {
+   res.send('Hello World');
+})*/
+app.use(express.static('www'));
+
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/www/" + "index.html" );
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
